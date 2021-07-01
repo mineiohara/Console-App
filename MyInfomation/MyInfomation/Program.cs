7deac9976b918319e19f50c4518024437ac3c4d4
Justin <mine1913@gmail.com>
2021-07-01 13:55:43 +0800

    chap2-1

---
+++ MyInfomation/MyInfomation/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MyInfomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name, studentID, grade;
+            name = "Name: Your name";
+            studentID = "Student ID: Your ID";
+            grade = "Grade: Your grade and department";
+            Console.WriteLine(name + "\n" + studentID + "\n" + grade);
+        }
+    }
+}
---
+++ SayHello/SayHello/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SayHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+
+        }
+    }
+}
