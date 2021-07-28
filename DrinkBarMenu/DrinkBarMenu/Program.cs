df486dc53027aa1f5c181ee0afcd081f6f1b5313
Justin <mine1913@gmail.com>
2021-07-28 12:21:15 +0800

    'class7-easy'

---
+++ DrinkBarMenu/DrinkBarMenu/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DrinkBarMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter base:");
+            string[] bases = Console.ReadLine().Split(' ');
+            Console.WriteLine("Enter fruit:");
+            string[] fruits = Console.ReadLine().Split(' ');
+
+            Console.WriteLine("Menu:");
+            for(int i = 0; i < bases.Length; i++)
+            {
+                for(int j = 0; j < fruits.Length; j++)
+                {
+                    Console.Write(fruits[j] + bases[i] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
