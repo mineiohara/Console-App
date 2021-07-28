077faf092fb88d39d63cce5b11c4a63364cba564
Justin <mine1913@gmail.com>
2021-07-29 00:41:51 +0800

    class-7 midium

---
+++ PascalTriangle/PascalTriangle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height, sum=0;
+
+            Console.Write("Please input height of the Pascal's triangle: ");
+            height = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < height; i++)
+            {
+                sum += (int)Math.Pow(2, i);
+            }
+
+            Console.Write(sum);
+        }
+    }
+}
