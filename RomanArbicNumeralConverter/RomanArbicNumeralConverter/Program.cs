5349e8c4cd4abd23e1df473a2d9c65573cd89e9f
Justin <mine1913@gmail.com>
2021-08-04 14:48:20 +0800

    class9

---
+++ RomanArbicNumeralConverter/RomanArbicNumeralConverter/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace RomanArbicNumeralConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var inputNum = Console.ReadLine();
+            bool Arabic = CheckRomanOrArbic(inputNum);
+
+            if (Arabic) ArabicToRoman(inputNum);
+            else RomanToArabic(inputNum);
+        }
+
+        static bool CheckRomanOrArbic(string input)//True -> Arabic, False -> Roman
+        {
+            bool result;
+            try
+            {
+                int.Parse(input);
+                result = true;
+            }
+            catch
+            {
+                result = false;
+            }
+
+            return result;
+        }
+
+        static int RomanToArabic(string input)
+        {
+
+        }
+
+        static string ArabicToRoman(int input)
+        {
+
+        }
+    }
+}
